5e0865ccd1a476a0d59b5d2bf6ba2cd1ef921d36
Jazim Abbas <jazimabbas87@gmail.com>
2023-02-13 20:39:39 +0500

    dep-injection: added channel repository interface

--- src/common/repositories/types/channel.ts
+++ src/common/repositories/types/channel.ts
@@ -1,3 +1,41 @@
+import { MessageStatus } from "../../../common/utils/app.type";
+import { ChannelModel } from "../../../common/models/types/channel";
+
+export type CreateChannelDto = {
+  userId: string;
+  opponentUserId: string;
+};
+
+export type IsChannelExistsPayload = CreateChannelDto;
+
+export type ChangeStatusDto = {
+  channelId: string;
+  userId: string;
+  messageStatus: MessageStatus;
+};
+
 export interface IChannelRepository {
-  createChannel(): Promise<any>;
+  /**
+   * Create Channel
+   * @param payload
+   */
+  createChannel(payload: CreateChannelDto): Promise<ChannelModel>;
+
+  /**
+   * Check if Channel already exists between users
+   * @param payload
+   */
+  isChannelExits(payload: IsChannelExistsPayload): Promise<boolean>;
+
+  /**
+   * Change message_status from 'SENT' to 'DELIVERED' or 'READ'
+   * @param payload
+   */
+  changeStatus(payload: ChangeStatusDto): Promise<void>;
+
+  /**
+   * Retrieve Channel from databse
+   * @param channelId
+   */
+  getChannel(channelId: string): Promise<ChannelModel>;
 }
